886eef53c9c3a3c1f5a726fdee76df8b65f126ee
Maurocoradoca <mauricioadriancoradoca@gmail.com>
2026-02-22 17:07:21 -0600

    Creacion de listfactory

--- ListFactory.java
+++ ListFactory.java
@@ -1,3 +1,18 @@
-public class ListFactory {
-    
+public class ListFactory<T> {
+
+    public IList<T> getList(String listType) {
+        
+        if (listType == null) {
+            return null;
+        }
+
+        if (listType.equalsIgnoreCase("SIMPLE")) {
+            return new SinglyLinkedList<T>();
+            
+        } else if (listType.equalsIgnoreCase("DOBLE")) {
+            return new DoublyLinkedList<T>();
+        }
+        
+        return null;
+    }
 }
