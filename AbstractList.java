9900e96b0cfbea80e3929742c0efe48e65722ca2
Maurocoradoca <mauricioadriancoradoca@gmail.com>
2026-02-22 16:55:21 -0600

    Creacion de la logica en AbstracList

--- AbstractList.java
+++ AbstractList.java
@@ -1,3 +1,17 @@
-public class AbstractList {
-    
+public abstract class AbstractList<T> implements IList<T> {
+    protected int size;
+
+    public AbstractList() {
+        this.size = 0; 
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
 }
