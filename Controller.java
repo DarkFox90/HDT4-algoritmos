b8f05e07674195ebc747b841c43f0160d4e48b16
Maurocoradoca <mauricioadriancoradoca@gmail.com>
2026-02-23 17:02:31 -0600

    Implementacion de conversion de infix a postfix

--- Controller.java
+++ Controller.java
@@ -18,7 +18,8 @@ public class Controller {
         try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while((line = reader.readLine()) != null) { 
-                double outcome = calculator.operate(line);
+                String postfix = infixToPostfix(line);
+                double outcome = calculator.operate(postfix);
                 System.out.println(outcome);
             } 
         } catch (FileNotFoundException e) {
@@ -27,5 +28,50 @@ public class Controller {
         catch(IOException e) {
             System.out.println("algo salio mal");
         }
+    }
+
+    private String infixToPostfix(String infix) {
+        StringBuilder postfix = new StringBuilder();
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+
+        for (int i = 0; i < infix.length(); i++) {
+            char x = infix.charAt(i);
+            if (Character.isLetterOrDigit(x)) {
+                postfix.append(x).append(" "); 
+            } 
+            else if (x == '(') {
+                stack.push(x);
+            } 
+            else if (x == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    postfix.append(stack.pop()).append(" ");
+                }
+                stack.pop(); 
+            } 
+            else if (x == '+' || x == '-' || x == '*' || x == '/') {
+                while (!stack.isEmpty() && precedence(x) <= precedence(stack.peek())) {
+                    postfix.append(stack.pop()).append(" ");
+                }
+                stack.push(x);
+            }
+        }
+        while (!stack.isEmpty()) {
+            postfix.append(stack.pop()).append(" ");
+        }
+        return postfix.toString().trim(); 
+    }
+
+    private int precedence(char x) {
+        switch (x) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+        }
+        return -1;
     }
 }
