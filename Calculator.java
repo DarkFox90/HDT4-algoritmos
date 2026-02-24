9091ac57781e890dec8da717e17c6b766704563f
Maurocoradoca <mauricioadriancoradoca@gmail.com>
2026-02-24 16:04:15 -0600

    Logica en los metodos de Calculator y algunas prubeas JUNit

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,50 @@
-public class Calculator {
+public class Calculator implements Calc {
 
     private Stack<Integer> stack;
-    private static Calc calculator;
-    
+    private static Calculator calculator;
+
+    private Calculator() {
+    }
+
+    public static Calculator getInstance() {
+        if (calculator == null) {
+            calculator = new Calculator();
+        }
+        return calculator;
+    }
+
+    public void PostfixCalculator(Stack<Integer> stack) {
+        this.stack = stack;
+    }
+
+    @Override
+    public double operate(String input) {
+        String[] tokens = input.split(" ");
+        for (String token : tokens) {
+            if (isNumeric(token)) {
+                stack.push(Integer.parseInt(token));
+            } else {
+                int b = stack.pop();
+                int a = stack.pop();
+                switch (token) {
+                    case "+":
+                        stack.push(a + b);
+                        break;
+                    case "-":
+                        stack.push(a - b);
+                        break;
+                    case "*":
+                        stack.push(a * b);
+                        break;
+                    case "/":
+                        stack.push(a / b);
+                        break;
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+    public boolean isNumeric(String num) {
+    }   
 }
---
+++ TestJUNit.java
@@ -0,0 +1,38 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class TestJUNit {
+
+    @Test
+    public void testPush() {
+        StackA<Integer> stack = new StackA<>();
+        stack.push(50);
+        
+        assertFalse(stack.isEmpty());
+        assertEquals(50, stack.peek());
+    }
+
+    @Test
+    public void testPop() {
+        StackA<Integer> stack = new StackA<>();
+        stack.push(10);
+        stack.push(20);
+        
+        int sacado = stack.pop();
+        assertEquals(20, sacado);
+        
+        assertEquals(10, stack.pop());
+    }
+
+    @Test
+    public void testIsEmpty() {
+        StackA<Integer> stack = new StackA<>();
+        assertTrue(stack.isEmpty());
+        
+        stack.push(99);
+        assertFalse(stack.isEmpty());
+        
+        stack.pop();
+        assertTrue(stack.isEmpty());
+    }
+}
