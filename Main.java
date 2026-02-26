f124a7be839d5e7b0323828fd29f2029343217fe
DarkFox90 <carlospozuelos3@gmail.com>
2026-02-25 18:12:31 -0600

    finalizado Main

--- Main.java
+++ Main.java
@@ -1,48 +1,62 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Calculator calculator = Calculator.getInstance();
         Controller controller = new Controller(calculator);
         StackFactory<Integer> factory1 = new StackFactory<>();
 
-    
-        System.out.println("Seleccione su implementacion de stack deseada: ");
-        System.out.println("1. ArrayList");
-        System.out.println("2. vector");
-        System.out.println("3. lista");
+        int opcion = 0;
 
-        int opcion = sc.nextInt();
-        Stack<Integer> stack = null;
+        while (opcion != 4) {
+            System.out.println("Seleccione su implementacion de stack deseada: ");
+            System.out.println("1. ArrayList");
+            System.out.println("2. vector");
+            System.out.println("3. lista");
+            System.out.println("4. salir");
 
-        switch (opcion) {
-            case 1:
-                stack = factory1.getStack("ArrayList", null);
-                break;
-            case 2:
-                stack = factory1.getStack("Vector", null);
-                break;
-            case 3:
-                System.out.println("que tipo de lista desea usar: ");
-                System.out.println("1. simplemente encadenada");
-                System.out.println("2. doblemente encadenada");
-                int opcion2 = sc.nextInt();
+            opcion = sc.nextInt();
+            Stack<Integer> stack = null;
 
-                if (opcion2 == 1) {
-                    
-                } else if (opcion2 == 2) {
+            switch (opcion) {
+                case 1:
+                    stack = factory1.getStack("ArrayList", null);
+                    break;
+                case 2:
+                    stack = factory1.getStack("Vector", null);
+                    break;
+                case 3:
+                    System.out.println("que tipo de lista desea usar: ");
+                    System.out.println("1. simplemente encadenada");
+                    System.out.println("2. doblemente encadenada");
+                    int opcion2 = sc.nextInt();
 
-                } else {
-                    System.out.println("opcion invalida.");
-                    sc.close();
-                }
-            
-            default:
-                throw new AssertionError();
+                    switch (opcion2) {
+                        case 1:
+                            stack = factory1.getStack("List", "SIMPLE");
+                            break;
+                        case 2:
+                            stack = factory1.getStack("List", "DOBLE");
+                            break;
+                        default:
+                            System.out.println("opcion invalida.");
+                            sc.close();
+                            return;
+                    }
+                    break;
+                case 4:
+                    System.out.println("Gracias por usar el programa");
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+
+            if (stack != null) {
+                calculator.PostfixCalculator(stack);
+                controller.ReadFile();
+            }
         }
-        calculator.PostfixCalculator(stack);
-        controller.ReadFile();
         sc.close();
     }
 }
